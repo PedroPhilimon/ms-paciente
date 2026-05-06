3469b8bbc5225fa446c2093c830a3c8df99f71a0
PabloDev <pe.philimon@duocuc.cl>
2026-05-06 19:45:09 -0400

    Se añadio una lista de pacientes y el metodo buscar pacientes

--- src/main/java/com/pacientes/servicio_pacientes/controller/PacienteController.java
+++ src/main/java/com/pacientes/servicio_pacientes/controller/PacienteController.java
@@ -1,10 +1,41 @@
 package com.pacientes.servicio_pacientes.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pacientes.servicio_pacientes.model.Paciente;
+import com.pacientes.servicio_pacientes.service.PacienteService;
+
 @RestController
-@RequestMapping
+@RequestMapping("api/v1/pacientes")
 public class PacienteController {
 
+    @Autowired
+    private PacienteService pacienteService;
+
+    @GetMapping
+    public ResponseEntity<List<Paciente>> listar() {
+        List<Paciente> pacientes = pacienteService.findAll();
+
+        if(pacientes.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(pacientes);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Paciente> buscar(@PathVariable Integer id) {
+        try {
+            Paciente paciente = pacienteService.findById(id);
+            return ResponseEntity.ok(paciente);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
