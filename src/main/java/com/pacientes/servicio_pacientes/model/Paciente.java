f4a67edd1faf04cb65b91234ad6e6044e483b0b6
PabloDev <pe.philimon@duocuc.cl>
2026-05-06 18:06:38 -0400

    Primer commit

---
+++ src/main/java/com/pacientes/servicio_pacientes/model/Paciente.java
@@ -0,0 +1,10 @@
+package com.pacientes.servicio_pacientes.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "paciente")
+public class Paciente {
+
+}
