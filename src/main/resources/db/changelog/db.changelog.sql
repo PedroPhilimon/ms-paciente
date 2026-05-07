54dd9ff5058f7591f0da0ed62974dab5aec156ca
PabloDev <pe.philimon@duocuc.cl>
2026-05-06 20:56:12 -0400

    se creo la base de datos y la tabla

---
+++ src/main/resources/db/changelog/db.changelog.sql
@@ -0,0 +1,9 @@
+CREATE TABLE paciente (
+    id_paciente BIGINT AUTO_INCREMENT PRIMARY KEY,
+    run VARCHAR(255),
+    nombre VARCHAR(255),
+    apellido VARCHAR(255),
+    fechaNacimiento VARCHAR(255),
+    prevision VARCHAR(255)
+    /*FOREIGN KEY (categoria_id) REFERENCES categoria(id_categoria)*/
+)
