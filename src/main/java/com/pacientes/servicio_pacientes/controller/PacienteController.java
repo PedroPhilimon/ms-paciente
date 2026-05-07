73043ce3f5c740e7c1864213d0ebc5472bebd151
PabloDev <pe.philimon@duocuc.cl>
2026-05-06 20:44:33 -0400

    Cambio realizado en el delete mapping

--- src/main/java/com/pacientes/servicio_pacientes/controller/PacienteController.java
+++ src/main/java/com/pacientes/servicio_pacientes/controller/PacienteController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -69,7 +70,7 @@ public ResponseEntity<Paciente> actualizar(@PathVariable Long id, @Valid @Reques
     }
 }
 
-@DeleteMapping("/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<?> eliminar(@PathVariable Long id) {
         try {
             pacienteService.delete(id);
