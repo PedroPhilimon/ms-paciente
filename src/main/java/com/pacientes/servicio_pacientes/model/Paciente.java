06c4478fdedfa24e19af65ace5c4ac7b51160cc5
PabloDev <pe.philimon@duocuc.cl>
2026-05-07 21:25:56 -0400

    se agregó la validacion notnull y notblank en el modelo de paciente

--- src/main/java/com/pacientes/servicio_pacientes/model/Paciente.java
+++ src/main/java/com/pacientes/servicio_pacientes/model/Paciente.java
@@ -7,6 +7,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,21 +22,28 @@ public class Paciente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    @NotNull
     @Column(name = "id_paciente")
     private Long id;
-
+    
+    @NotBlank(message = "El RUN es obligatorio")
     @Column(nullable = false) 
     private String run;
 
+    @NotBlank(message = "El nombre es obligatorio")
     @Column(nullable = false)
     private String nombre;
 
+    @NotBlank(message = "El apellido es obligatorio")
     @Column(nullable = false)
     private String apellido;
 
+    @NotNull(message = "la fecha de nacimiento es obligatoria")
     @Column(name = "fecha_nacimiento")
     private LocalDate fechaNacimiento;
 
+    @NotNull(message = "la previsión es obligatoria")
     @Column(nullable = false)
     private String prevision;
 
