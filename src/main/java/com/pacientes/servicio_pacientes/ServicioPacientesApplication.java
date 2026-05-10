b8aa7264e9ed34df92889ba4ce9d47deed88c12b
PabloDev <pe.philimon@duocuc.cl>
2026-05-10 15:16:09 -0400

    se agregó el enablediscoveryClient

--- src/main/java/com/pacientes/servicio_pacientes/ServicioPacientesApplication.java
+++ src/main/java/com/pacientes/servicio_pacientes/ServicioPacientesApplication.java
@@ -2,7 +2,9 @@ package com.pacientes.servicio_pacientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServicioPacientesApplication {
 
 	public static void main(String[] args) {
