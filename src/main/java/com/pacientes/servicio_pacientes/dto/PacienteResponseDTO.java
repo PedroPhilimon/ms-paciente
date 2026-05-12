93a0e4f975de333ca314405be1208ddd0367543f
PabloDev <pe.philimon@duocuc.cl>
2026-05-12 16:56:36 -0400

    se modifico PacienteResponseDTO

--- src/main/java/com/pacientes/servicio_pacientes/dto/PacienteResponseDTO.java
+++ src/main/java/com/pacientes/servicio_pacientes/dto/PacienteResponseDTO.java
@@ -2,6 +2,8 @@ package com.pacientes.servicio_pacientes.dto;
 
 import java.time.LocalDate;
 
+import com.pacientes.servicio_pacientes.model.Paciente;
+
 import lombok.Data;
 
 @Data
@@ -12,4 +14,21 @@ public class PacienteResponseDTO {
     private String apellido;
     private LocalDate fechaNacimiento;
     private String prevision;
+
+
+
+
+
+
+
+    public static PacienteResponseDTO fromEntity(Paciente paciente) {
+        PacienteResponseDTO dto = new PacienteResponseDTO();
+        dto.setId(paciente.getId());
+        dto.setRun(paciente.getRun());
+        dto.setNombre(paciente.getNombre());
+        dto.setApellido(paciente.getApellido());
+        dto.setFechaNacimiento(paciente.getFechaNacimiento());
+        dto.setPrevision(paciente.getPrevision());
+        return dto;
+    }
 }
